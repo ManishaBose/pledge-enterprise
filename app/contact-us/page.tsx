35ceb7f93b840de89ec68680dfbcd5e07d315214
MANISHA BOSE <manishabose.2001@gmail.com>
2025-10-04 23:20:05 +0530

    Contact Us details added

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <div>Contact Us!</div>;
+  return (
+    <div>
+      <div>We’re always here to help.</div>
+      <div>
+        Whether you’re looking to place an order, explore our product range, or
+        have any inquiries, reach out — our team will get back to you promptly.
+      </div>
+      <div>Get in Touch</div>
+      <div>📞 Phone: 9818257268</div>
+      <div>🏢 Address: B-49, Harder Nagar, Delhi – 110084 </div>
+      <div>🕓 Office Hours: 10:00 AM – 6:00 PM</div>
+      <div>Business Details</div>
+      <div>
+        Director: Ritesh Gupta GST No.: 07ACUPG9306M1ZZ MSME Status: Registered
+      </div>
+    </div>
+  );
 }
